cdc504df805c784b32c87b14c2c9a373d1bac477
Simeon013 <karlsnow000@gmail.com>
2021-11-21 20:23:36 +0100

    creation de l'activité Calculatrice

---
+++ app/src/main/java/Simeon013/demo/Calculatrice.java
@@ -0,0 +1,14 @@
+package Simeon013.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculatrice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
